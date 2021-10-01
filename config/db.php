3bd3646674431ea938de1cb3b5529e0cea342bfa
hoochicken <mareike.riegel@ql.de>
2021-10-01 13:01:11 +0200

    initiating

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mary;dbname=time',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
