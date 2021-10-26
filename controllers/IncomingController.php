e19d8487fb9519016a03ac1a094d20096d63c61f
hoochicken <mareike.riegel@ql.de>
2021-10-26 14:03:48 +0200

    incoming: saving invoice stores incoice numver in workingtime entry

--- controllers/IncomingController.php
+++ controllers/IncomingController.php
@@ -11,6 +11,7 @@ use app\models\WorkingtimeSearch;
 use http\Exception;
 use yii\base\BaseObject;
 use yii\data\DataFilter;
+use yii\db\ActiveRecord;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -99,31 +100,37 @@ class IncomingController extends Controller
         $userModels = new UserSearch();
         $userProvider = $userModels->search([]);
 
-        $model->setAttribute('cid', $customerId);
-        $model->setAttribute('invoice_date', date('Y-m-d'));
-        $model->setAttribute('last_update', date('Y-m-d H:i:s'));
-        $model->setAttribute('create_date', date('Y-m-d H:i:s'));
-
-        $minutes = (int) $workingtimeModels->sumUpMinutes(['WorkingtimeIds' => $selectedIds]);
-        $hours = $minutes / 60;
-
-        $goods_sales = (float) ($customerModel->salary ?? null) * (float) $hours;
-        $tax_value = 0; // steuersatz
-        $sales_tax = $goods_sales * $tax_value; // steuerbetrag
-        $gross = $goods_sales + $sales_tax; // gesamter abzurechnender betrag
-
-        // $model->setAttribute('minutes_original', $minutes);
-        // $model->setAttribute('minutes', $minutes);
-        $model->setAttribute('gross', $gross);
-        $model->setAttribute('tax_value', $tax_value);
-        $model->setAttribute('sales_tax', $sales_tax);
-        $model->setAttribute('goods_sales', $goods_sales);
-
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
                 $model->setAttribute('last_update', date('Y-m-d H:i:s'));
+
+                // save invoice id in workingtime entry
+                $idsToBeMarkedAsDone = $this->request->getBodyParam('selection', []);
+                $workingtimeProvider2 = $workingtimeModels->search(['WorkingtimeIds' => $idsToBeMarkedAsDone]);
+                /** @var $workingtime ActiveRecord */
+                foreach ($workingtimeProvider2->getModels() as $workingtime) {
+                    $workingtime->setAttribute('invoice_number', $model->id);
+                    $workingtime->save(false);
+                }
                 return $this->redirect(['view', 'id' => $model->id]);
             }
+            $model->setAttribute('cid', $customerId);
+            $model->setAttribute('invoice_date', date('Y-m-d'));
+            $model->setAttribute('last_update', date('Y-m-d H:i:s'));
+            $model->setAttribute('create_date', date('Y-m-d H:i:s'));
+
+            $minutes = (int) $workingtimeModels->sumUpMinutes(['WorkingtimeIds' => $selectedIds]);
+            $hours = $minutes / 60;
+
+            $goods_sales = (float) ($customerModel->salary ?? null) * (float) $hours;
+            $tax_value = 0; // steuersatz
+            $sales_tax = $goods_sales * $tax_value; // steuerbetrag
+            $gross = $goods_sales + $sales_tax; // gesamter abzurechnender betrag
+
+            $model->setAttribute('gross', $gross);
+            $model->setAttribute('tax_value', $tax_value);
+            $model->setAttribute('sales_tax', $sales_tax);
+            $model->setAttribute('goods_sales', $goods_sales);
         } else {
             $model->loadDefaultValues();
         }
